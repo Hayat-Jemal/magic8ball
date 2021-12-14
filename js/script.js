eb8c46a84e51b0a1632723fed0f912d61e587d56
Hayat Jemal <htjemal2000@gmail.com>
2021-12-15 00:07:35 +0300

    finished project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-const title = document.getElementById("title")
-// const mgc8bl = document.getElementById("wrapper-mgc8ball");
+const title = document.getElementById("title");
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const answer = document.getElementById("answers");
 const backBtn = document.createElement("button");
+const content = document.getElementById("content");
 
-button.addEventListener("click", ask);
-
-// a function that displays random magic8ball image when ask button clicked
 let rot = 360;
-function ask() {
-  const imgArray = [
+button.addEventListener("click", function ask() {
+  let allImg = [
     "images/magic8ball_1.png",
     "images/magic8ball_2.png",
     "images/magic8ball_3.png",
@@ -32,53 +29,45 @@ function ask() {
     "images/magic8ball_19.png",
     "images/magic8ball_20.png",
   ];
-  if (input.value.length != 0) {
-   
-    // selecting random index from imgArray
-    const randomImgIndex = Math.floor(Math.random() * imgArray.length);
-    console.log(randomImgIndex);
+
+  if (input.value.length !== 0) {
+    // selecting random index from allImg
+    let randomImg = Math.floor(Math.random() * allImg.length);
+    console.log(randomImg);
+
     // Create image tag to insert random image
-    const img = document.createElement("img");
-    // img.src = imgArray[getRandomImage()];
-    img.src = imgArray[randomImgIndex];
+    let img = document.createElement("img");
+    img.src = allImg[randomImg];
     // append selected random image into html
     answer.appendChild(img);
 
-    // hide input field and ask button
-    
+    content.style.display = "none";
+    title.style.display = "none";
+
+    // create return button to ask another
+    backBtn.setAttribute("id", "backBtn");
+    //with out "id" js works but we want to add css styles
+    backBtn.innerText = "Ask more questions";
+    answer.appendChild(backBtn);
+    backBtn.style.textAlign = "center";
+
+    // create p tag and append to html to display users question after user clicked ask button
+    let asked = document.createElement("p");
+    asked.setAttribute("id", "text");
+    asked.innerText = input.value;
+    answer.prepend(asked);
+
+    backBtn.addEventListener("click", () => {
+      content.style.display = "block";
+      title.style.display = "block";
+      input.value = "";
+      answer.innerHTML = "";
+    });
   } else {
-    alert("Enter a question.");
-   
+    alert("please enter a question.");
   }
-  backBtn.addEventListener("click", function returnToAsk() {
-    content.style.display = "block";
-    input.value = "";
-     title.style.display = "block";
-    answer.innerHTML = "";
-  });
-  const content = document.getElementById("content");
-  content.style.display = "none";
-  title.style.display = "none";
-
-  // create p tag and append to html to display users question after user clicked ask button
-  const askedQuestion = document.createElement("p");
-  askedQuestion.setAttribute("id", "text");
-  answer.prepend(askedQuestion);
-  askedQuestion.innerText = input.value;
-
-  // // create return button to ask another
-  backBtn.setAttribute("id", "backBtn");
-  //with out "id" js works but we want to add css styles
-  backBtn.innerText = "Ask more questions";
-  backBtn.style.textAlign = "center";
-  answer.appendChild(backBtn);
-
   // rotate the answer
-  answer.style.transition = "all 1s ease-in-out";
   answer.style.transform = "rotate(" + rot + "deg)";
+  answer.style.transition = "all 1s ease-in-out";
   rot += 360;
-     
-
-}
-
-
+});
